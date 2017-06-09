be0af4fd173e1f7f9d8debc10562eab4c8caedce
Brandon Mills <sllimnodnarb@gmail.com>
2017-06-09 09:24:55 -0500

    adding all files associated with user-story-5-api-controller

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,43 @@
+class API::EventsController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+  before_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  end
+
+  def preflight
+    head 200
+  end
+
+  def index
+    if request.method == "OPTIONS"
+     render :json => '', :content_type => 'application/json'
+     return
+    end
+  end
+
+  def create
+    registered_application = RegisteredApplication.find_by(url: request.env['HTTP_LOCALHOST:3000'])
+    if registered_application.nil?
+      render json: "Unregistered application", status: :unprocessable_entity
+    else
+      @event = registered_application.events.new(event_params)
+      if @event.save
+        render json: @event, status: :created
+      else
+        render json: @event.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,10 @@ Rails.application.routes.draw do
   get 'welcome/about'
 
   root 'welcome#index'
+
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
 end
